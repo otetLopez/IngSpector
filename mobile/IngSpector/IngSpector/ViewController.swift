9e2e9c0ecbc83ee2c5edaf35848ff9ee7dd76b92
otetLopez <otet.tud@gmail.com>
2020-04-07 16:20:50 -0400

    [Mobile] Add initial XCode files

---
+++ mobile/IngSpector/IngSpector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IngSpector
+//
+//  Created by otet_tud on 4/7/20.
+//  Copyright © 2020 otet_tud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
