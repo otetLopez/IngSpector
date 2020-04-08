579bf887b19dc03d91ca95b4a6d276706f3de3f5
otetLopez <otet.tud@gmail.com>
2020-04-07 20:53:22 -0400

    Add Layout for register & Login Page

---
+++ mobile/IngSpector/IngSpector/FirstPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstPageViewController.swift
+//  IngSpector
+//
+//  Created by otet_tud on 4/7/20.
+//  Copyright © 2020 otet_tud. All rights reserved.
+//
+
+import UIKit
+
+class FirstPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
