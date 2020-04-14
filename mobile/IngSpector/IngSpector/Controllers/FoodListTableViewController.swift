e8e2d5a7ddf19e9ee0836061126d1f69eeb3de7d
otetLopez <otet.tud@gmail.com>
2020-04-14 17:20:31 -0400

    [Mobile] Adding Delete Function in FoodList

--- mobile/IngSpector/IngSpector/Controllers/FoodListTableViewController.swift
+++ mobile/IngSpector/IngSpector/Controllers/FoodListTableViewController.swift
@@ -57,17 +57,19 @@ class FoodListTableViewController: UITableViewController {
     }
     */
 
-    /*
-    // Override to support editing the table view.
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            // Remove data from server
+            
+            // Update userdefaults?
+            
+            foodList.remove(at: indexPath.row)
+        }
     }
-    */
+
 
     /*
     // Override to support rearranging the table view.
