59d5d9a43e74243f690ed615545c7a18d39148e3
otetLopez <otet.tud@gmail.com>
2020-04-16 18:13:43 -0400

    [Mobile] Make Changes with Storyboard

--- mobile/IngSpector/IngSpector/Controllers/AnalyzePhotoViewController.swift
+++ mobile/IngSpector/IngSpector/Controllers/AnalyzePhotoViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class AnalyzePhotoViewController: UIViewController {
 
+    @IBOutlet weak var cameraBtn: UIButton!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var resultLbl: UILabel!
+    @IBOutlet weak var scanBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configureView()
     }
     
+    @IBAction func cameraBtnPressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func ScanBtnPressed(_ sender: UIButton) {
+    }
+    
+    
     func configureView() {
+        navigationItem.largeTitleDisplayMode = .never
         self.navigationController?.setNavigationBarHidden(false, animated: true)
         self.navigationItem.titleView?.isHidden = true
         self.navigationController?.setToolbarHidden(true, animated: true)
+        
+        cameraBtn.layer.cornerRadius = 10
+        scanBtn.layer.cornerRadius = 10
     }
 
 }
